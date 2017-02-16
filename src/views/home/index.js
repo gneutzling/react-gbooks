917375cab025d7618ea71580252a9958eb36e3eb
Gabriel Neutzling <gabrielneutzling@gmail.com>
2017-02-16 09:04:26 -0200

    Add home view.

---
+++ src/views/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Search from '../../modules/search';
+
+const Home = () => (
+  <div className="home">
+    <h1>Search Books</h1>
+    <Search />
+  </div>
+);
+
+export default Home;
